f57a841010fe8ab99c55520fb190d524fa168161
PriGig <120448753+PriGig@users.noreply.github.com>
2025-05-18 17:55:47 +0530

    Add files via upload

---
+++ bot/config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  PORT: 3980,
+  BOT_ID: "xxxxx",
+  BOT_PASSWORD: "Gxxx",
+  FEEDBACK_PATH: "../mcp-server/processed_feedback.json",
+  OAUTH_CONNECTION_NAME: "TeamsAuth"
+};
